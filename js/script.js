7a99bedb3cd9e3020d9639e5415b93f9287b9d18
ShCodeNick <averteastra@yandex.ru>
2025-11-09 20:36:33 +0300

    first homework

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log(document.querySelector('header'))
+const contents = document.querySelectorAll('.program-line__content')
+const allDescriptions = document.querySelectorAll('.program-line__descr')
+
+contents.forEach((elem) => {
+    const title = elem.querySelector('.program-line__title')
+
+    title.addEventListener('click', () => {
+        const hasActive = Array.from(allDescriptions).some(descr => descr.classList.contains('active'))
+
+        allDescriptions.forEach((descr) => {
+            if (hasActive) {
+                descr.classList.remove('active')
+            } else {
+                descr.classList.add('active')
+            }
+        })
+    })
+})
